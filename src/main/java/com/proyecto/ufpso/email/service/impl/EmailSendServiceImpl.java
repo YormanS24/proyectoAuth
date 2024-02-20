0b4acc57ab7e0ff8bc5cc6c3c794b3028cab9690
YormanS24 <sanchezyorman0@gmail.com>
2024-02-20 09:29:23 -0500

    Ajustes en el modulo de email

--- src/main/java/com/proyecto/ufpso/email/service/impl/EmailSendServiceImpl.java
+++ src/main/java/com/proyecto/ufpso/email/service/impl/EmailSendServiceImpl.java
@@ -3,6 +3,7 @@ package com.proyecto.ufpso.email.service.impl;
 import com.proyecto.ufpso.email.dto.EmailRequest;
 import com.proyecto.ufpso.email.service.EmailSendService;
 import com.proyecto.ufpso.email.service.EmailService;
+import com.proyecto.ufpso.user.entity.User;
 import org.springframework.stereotype.Service;
 
 import java.util.Properties;
@@ -22,6 +23,17 @@ public class EmailSendServiceImpl implements EmailSendService {
                 "sanchezyorman0@gmail.com",
                 "Codigo de Verificacion",
                 "Su coigo de Verificacion del CINE LEONELDA: "+codeVerification,
+                null,
+                null
+        ));
+    }
+
+    @Override
+    public void resetPasswordUser(User user,String urlToken) {
+        emailService.sendEmail(EmailRequest.create(
+                user.getPerson().getEmail(),
+                "Restablecer tu Contraseña",
+                "link_recovery_password"+urlToken,
                 null,
                 null
         ));
